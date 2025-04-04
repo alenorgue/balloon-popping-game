6cb087d9672f9ba68c373e65096b1ade3dfb4177
alenorgue <alenoriegag@gmail.com>
2025-04-04 11:12:17 +0200

    finished

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// tu código aquí
+let poppedBallon = 0;
+const totalBalloons = 24;
+const balloons = document.getElementById('balloon-gallery');
+const leftCounter = document.getElementById('left');
+const message = document.getElementById('yay-no-balloons');
+
+
+leftCounter.textContent = totalBalloons;
+
+balloons.addEventListener('mouseover', function (e) {
+    if (e.target.classList.contains('balloon')) {
+        balloonPop(e);
+    }
+});
+
+function balloonPop(e) {
+    e.target.style.backgroundColor = "#ededed";
+    e.target.textContent = "POP!";
+    e.target.classList.remove('balloon'); 
+    poppedBallon++;
+
+    leftCounter.textContent = totalBalloons - poppedBallon;
+
+    checkGameEnd();
+}
+
+function checkGameEnd() {
+    if (poppedBallon === totalBalloons) {
+        balloons.innerHTML = ''; 
+        message.style.display = 'block'; 
+    }
+}
